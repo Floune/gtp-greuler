1562d95981d59379bcbd8c43870256c0c2e72f06
RobinFloune <rfournier@simplonprod.co>
2021-03-03 23:37:09 +0100

    hdkuhakzdazd

--- index.js
+++ index.js
@@ -1,47 +1,25 @@
-const axios = require("axios");
 import greuler from 'greuler'
+const axios = require("axios");
 let instance;
-let data;
-let conf;
-let iterator = 1;
+let history;
+var diff = require("fast-array-diff");
 
-function init() {
-    axios.get("https://get-to-philosophy.herokuapp.com/history").then(history => {
-        data = history.data.words
-        instance = greuler(makeConf(take(data, iterator)))
-        instance.update()
-        watchers()
-    })
-
-    const then = function() {
-        debugger
-        iterator++
-        //instance = greuler()
-        const nodes = data[iterator].map(()=>{
-            createNode("Connaissance_(philosophie)", 80, "red")
-            }
-        )
-        instance.options.data.nodes.push
-        instance.update()
-
-        return then
-    }
-
-    return then
+function inite(words) {
+    const conf = makeConf(words)
+    instance = greuler(conf).update()
+    watchers()
 }
 
-function take(arr, count) {
-    return arr.slice(0, count);
-}
-
-
-window.init = init()
 
 function watchers() {
+    document.querySelector(".form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        fetchWord(e.target[0].value)
+    })
+
     document.body.addEventListener("click", (e) => {
         if (e.target.localName === "circle" || e.target.localName === "text") {
             console.log("kikou", e.target)
-            //debugger
         }
     })
 }
@@ -57,45 +35,47 @@ function makeConf(words) {
             edges: [],
         },
     }
-    const initialNode = createNode("Connaissance_(philosophie)", 80, "red")
+    const initialNode = createNode("Connaissance_(philosophie)", 80, "#00bbf9")
     const aggregateNodes = ["Connaissance_(philosophie)"]
     const aggregateEdges = []
     const nodes = [initialNode]
     const edges = []
     let previous;
-
-    const limit = 20
-    for (let i = 0; i < words.length; i++){
+    const limit = 100
+    let keys = Object.keys(words)
+    for (let i = 0; i < keys.length; i++){
         if (i === limit) {
             break;
         }
         previous = initialNode;
-        const key = Object.keys(words[i])[0]
-        const defs = words[i][key]
+        const key = keys[i]
+
+        const defs = words[key]
         for (let j = defs.length - 1; j >= 0; j--) {
             const edgeName = `${previous.id}-${defs[j]}`
-            if (aggregateEdges.indexOf(edgeName) === -1) {
+            if (aggregateEdges.indexOf(edgeName) === -1 && defs[j] !== "Entrée inexistante" && defs[j] !== "arret") {
                 edges.push(createEdge(defs[j],previous.id))
                 aggregateEdges.push(edgeName)
             }
-            previous = {id: defs[j]}
             if (aggregateNodes.indexOf(defs[j]) === -1) {
-                j === 1 ? nodes.push(createNode(defs[j], 35, "pink")) : nodes.push(createNode(defs[j]))
+                j === 0 ? nodes.push(createNode(defs[j], 35, "#f15bb5")) : nodes.push(createNode(defs[j]))
                 aggregateNodes.push(defs[j])
             }
+            previous = {id: defs[j]}
+
         }
 
     }
 
     return {
-    ...conf,
-    ...{
-        data: {nodes, edges}
-    }
+        ...conf,
+        ...{
+            data: {nodes, edges}
+        }
     }
 }
 
-function createNode(elem, size = 35, color = "blue") {
+function createNode(elem, size = 35, color = "#fee440") {
     return {
         fill: color,
         id: decodeURI(elem),
@@ -107,9 +87,66 @@ function createNode(elem, size = 35, color = "blue") {
 
 
 function createEdge(source, target) {
+    console.log("edge", source, target)
     return {
         source: decodeURI(source),
         target: decodeURI(target),
         directed: true,
     }
 }
+
+function updateGraph(old) {
+    axios.get("https://get-to-philosophy.herokuapp.com/history").then(newList => {
+        document.getElementById("loading").style.display = "none"
+        const aggregateNodes = instance.graph.nodes
+        const aggregateEdges = []
+        let intersection = diff.diff(Object.keys(old), Object.keys(newList.data.words));
+        let newBranch = newList.data.words[intersection["added"][0]]
+        let previous = {id: "Connaissance_(philosophie)"}
+        for (let j = newBranch.length - 1; j >= 0; j--) {
+            if (existed(newBranch[j], aggregateNodes) === false) {
+                j === 0 ? instance.graph.addNode(createNode(newBranch[j], 35, "#f15bb5")) : instance.graph.addNode(createNode(newBranch[j]))
+            }
+            const edgeName = `${previous.id}-${newBranch[j]}`
+            if (aggregateEdges.indexOf(edgeName) === -1 && newBranch[j] !== previous.id && newBranch[j] !== "Entrée inexistante" && newBranch[j] !== "arret") {
+                instance.graph.addEdge(createEdge(newBranch[j],previous.id))
+                aggregateEdges.push(edgeName)
+            }
+
+
+            previous = {id: newBranch[j]}
+
+        }
+        instance.update()
+
+    })
+}
+
+function existed(check, history) {
+    let aexisted = false
+    history.forEach(item => {
+        if (decodeURI(check) === item.id) {
+            aexisted =  true
+        }
+    })
+    return aexisted
+}
+
+function fetchWord(word) {
+    document.getElementById("loading").style.display = "block"
+    axios.get("https://get-to-philosophy.herokuapp.com/history").then(old => {
+        axios.get("https://get-to-philosophy.herokuapp.com/gtp?s=" + word).then((res) => {
+            updateGraph(old.data.words)
+        })
+    })
+}
+
+
+function start() {
+    axios.get("https://get-to-philosophy.herokuapp.com/history").then(history => {
+        history = history.data.words
+        inite(history)
+    })
+}
+
+start()
